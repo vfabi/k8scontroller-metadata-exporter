20df83cc400499e8a4829e687ae0ebede87dbe34
fabi <fabi@example.com>
2020-05-31 21:55:35 +0300

    small refactoring updates

--- app.py
+++ app.py
@@ -5,9 +5,8 @@
     @project: k8s-controller-objects-metadata
     @component: core
     @author: vfabi
-    @contact person: vfabi
     @support: vfabi
-    @inital date: 2020-05-17 18:30:40
+    @initial date: 2020-05-17 18:30:40
     @license: this file is subject to the terms and conditions defined
         in file 'LICENSE', which is part of this source code package
     @description
